21bdfe46f8751801b5a71c32553e892c66b61295
Steve Roth <steve@sidestudios.com>
2015-02-18 11:30:42 -0800

    Update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="django-bazaarvoice",
     version="0.1",
     url="https://github.com/SideStudios/django-bazaarvoice",
-    packages=["django-bazaarvoice"],
+    packages=["bvseosdk"],
     license="",
     description="Django integration for BazaarVoice SEO",
     long_description=open("README.md").read(),
