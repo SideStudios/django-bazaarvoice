799886ebb02f55c13b2b2248ca83cee71fd28fd2
Steve Roth <steve@sidestudios.com>
2015-02-18 11:02:14 -0800

    Copy from BazaarVoice django example

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="django-bazaarvoice",
+    version="0.1",
+    url="https://github.com/SideStudios/django-bazaarvoice",
+    packages=["django-bazaarvoice"],
+    license="",
+    description="Django integration for BazaarVoice SEO",
+    long_description=open("README.md").read(),
+)
